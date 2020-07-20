3a9595c7d33206ea42116656a3c7a5e8056c26d0
kisel-dv <timcaiser@gmail.com>
2020-07-20 17:19:51 +0300

    removed proxy using because of telegram permit

--- fantasyBot.py
+++ fantasyBot.py
@@ -7,24 +7,6 @@ import time
 from config import TOKEN, CHANNEL_ID, PROXY_LIST, SELF_CHAT_ID
 
 
-def safety_send_group(channel_id, media, proxy=None):
-    bot = telebot.TeleBot(TOKEN)
-    if proxy is not None:
-        telebot.apihelper.proxy = {'https': proxy}
-    for i in range(5):
-        try:
-            bot.send_media_group(channel_id, media)
-            logging.info('Статистика по чемпионату отправлена в канал')
-            return
-        except:
-            logging.warning(
-                'Ошибка при попытке отправки сообщения в канал, попытка {}'.format(i + 1))
-            time.sleep(1)
-            continue
-    logging.error('Запостить сообщение не вышло :(')
-    raise Exception
-
-
 def check_proxy():
     bot = telebot.TeleBot(TOKEN)
     for current_proxy in range(len(PROXY_LIST)):
@@ -44,6 +26,25 @@ def check_proxy():
     raise Exception
 
 
+def safety_send_group(channel_id, media, proxy=False):
+    bot = telebot.TeleBot(TOKEN)
+    if proxy:
+        working_proxy = check_proxy()
+        telebot.apihelper.proxy = {'https': working_proxy}
+    for i in range(5):
+        try:
+            bot.send_media_group(channel_id, media)
+            logging.info('Статистика по чемпионату отправлена в канал')
+            return
+        except:
+            logging.warning(
+                'Ошибка при попытке отправки сообщения в канал, попытка {}'.format(i + 1))
+            time.sleep(1)
+            continue
+    logging.error('Запостить сообщение не вышло :(')
+    raise Exception
+
+
 # kwargs Для channel_id/proxy
 def posting_to_channel(caption, *files, **kwargs):
     channel_id = CHANNEL_ID if kwargs.get('channel_id') is None else kwargs['channel_id']
@@ -52,6 +53,6 @@ def posting_to_channel(caption, *files, **kwargs):
         media = []
         for i in range(len(pics)):
             media.append(InputMediaPhoto(pics[i], caption) if i == 0 else InputMediaPhoto(pics[i]))
-        working_proxy = check_proxy()
-        safety_send_group(channel_id, media, working_proxy)
+        # теперь можно и без прокси
+        safety_send_group(channel_id, media, proxy=False)
     return
